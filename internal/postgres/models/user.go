439287361ecbec5df4219173c1f208c09a667144
Misael Alisson Ursulino de Lima <maul1@aluno.ifal.edu.br>
2025-03-02 11:18:33 -0300

    chore: remove unused sqlc file and db tags

--- internal/postgres/models/user.go
+++ internal/postgres/models/user.go
@@ -6,15 +6,15 @@ import (
 )
 
 type User struct {
-	ID               uuid.UUID `db:"ID" json:"id"`
-	Username         string    `db:"username" json:"username"`
-	FirstName        string    `db:"first_name" json:"first_name"`
-	LastName         string    `db:"last_name" json:"last_name"`
-	Password         string    `db:"password_hash" json:"password"`
-	Email            string    `db:"email" json:"email"`
-	ProfilePicture   string    `db:"profile_picture" json:"profile_picture"`
-	Income           float64   `db:"income" json:"income"`
-	ExpenditureLimit float64   `db:"expenditure_limit" json:"expenditure_limit"`
-	CreatedAt        time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt        time.Time `db:"updated_at" json:"updated_at"`
+	ID               uuid.UUID `json:"id"`
+	Username         string    `json:"username"`
+	FirstName        string    `json:"first_name"`
+	LastName         string    `json:"last_name"`
+	Password         string    `json:"password"`
+	Email            string    `json:"email"`
+	ProfilePicture   string    `json:"profile_picture"`
+	Income           float64   `json:"income"`
+	ExpenditureLimit float64   `json:"expenditure_limit"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
