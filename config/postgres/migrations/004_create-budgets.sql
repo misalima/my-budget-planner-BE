de727d7f032734787fbffdbde8c4edf1b3499340
Misael Alisson Ursulino de Lima <maul1@aluno.ifal.edu.br>
2025-03-02 22:44:45 -0300

    moves migrations to config folder, and rewrites migrations to reflect new database schema

--- config/postgres/migrations/004_create-budgets.sql
+++ config/postgres/migrations/004_create-budgets.sql
@@ -1,15 +1,11 @@
 CREATE TABLE IF NOT EXISTS budgets
 (
-    "ID" serial NOT NULL,
+    "ID" serial PRIMARY KEY NOT NULL,
     user_id uuid NOT NULL,
     budget_name character varying(255) NOT NULL,
     description character varying(255),
     amount double precision NOT NULL,
-    category_id integer NOT NULL,
-    start_date date NOT NULL,
-    end_date date NOT NULL,
-    period character varying(255) NOT NULL,
-    PRIMARY KEY ("ID")
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users ("ID") ON DELETE CASCADE
 );
 ---- create above / drop below ----
 
